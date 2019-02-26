f7f5614c04beecff3d025773a5d95332f1d65dcd
krazykoder <noddy4nods@gmail.com>
2019-02-26 14:36:45 -0800

    Add project files.

---
+++ rssiHistoryDB/model/trackerJSON.js
@@ -0,0 +1,26 @@
+﻿var mongoose = require('mongoose');
+var blobSchema = new mongoose.Schema({
+    time : { type: Date, default: Date.now },
+    location: String,
+    BID: String,
+    SID: String,
+    FID: { type: String, default: "NA" },
+    rssi: Number,
+    isFlagged: { type: Boolean, default: false }
+});
+mongoose.model('TrackerJSON', blobSchema);
+
+
+/*
+
+[
+   {
+      "location":"5W",
+      "BID":"Ping",
+      "SID":"5W-SENS5",
+      "rssi":0,
+      "version":"2.0.9"
+   },
+
+
+*/
